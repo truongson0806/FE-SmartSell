de2195b001a876475e6ba975ca24de55accd066f
lehuyhoang04 <hoangdepzai462004@gmail.com>
2026-04-02 09:54:03 +0700

    Add Message component for chat interface

--- src/components/AIChat/Message.jsx
+++ src/components/AIChat/Message.jsx
@@ -1,1 +1,19 @@
+import React from "react";
 
+const Message = ({ msg }) => {
+  return (
+    <div className={`flex ${msg.type === "user" ? "justify-end" : "justify-start"}`}>
+      <div
+        className={`p-2 rounded-xl max-w-[70%] text-sm ${
+          msg.type === "user"
+            ? "bg-blue-500 text-white"
+            : "bg-gray-200 text-black"
+        }`}
+      >
+        {msg.text}
+      </div>
+    </div>
+  );
+};
+
+export default Message;
